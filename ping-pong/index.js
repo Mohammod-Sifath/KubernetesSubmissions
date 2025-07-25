c1cb83166c16f7cd92cfa49eb1f0dafdfc2ec459
AKM Sifath Ullah <md.sifath93@gmail.com>
2025-07-25 03:32:29 +0300

    Exercise 1.9 complete:pingpong app

---
+++ ping-pong/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+let counter = 0;
+
+app.get('/pingpong', (req, res) => {
+  counter += 1;
+  res.send(`pong ${counter}`);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Pingpong app running on port ${PORT}`);
+});
